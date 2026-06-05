2d4dc5f20fe1b5ad3a5148ed7b8dd61f1855d29a
Yanislav Igonin <yanislavdabest@gmail.com>
2026-06-05 14:49:05 +0800

    distribution tests

---
+++ tests/seeded-random.ts
@@ -0,0 +1,9 @@
+import type { RandomSource } from "../src/random.js";
+
+export function createSeededRandom(seed: number): RandomSource {
+  let state = seed >>> 0;
+  return () => {
+    state = (state * 1664525 + 1013904223) >>> 0;
+    return state / 0x100000000;
+  };
+}
